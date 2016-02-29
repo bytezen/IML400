5b1af7d2318e0e53283f0015b298cb0a0bc730d2
sunrha <admin@sunrha>
2016-02-29 10:59:07 -0800

    starting whodat03 to demo object configuration

---
+++ workshops/whodat_03/public/scripts/whodat.js
@@ -0,0 +1,28 @@
+console.log("loading whodat");
+
+var testConfig = {
+beak : {},
+eye_L : {},
+eye_R: {},
+pupil_L: {},
+pupil_R: {},
+leg_L : {},
+leg_R : {},
+ear_L : {},
+ear_R : {}
+
+}
+
+
+function makeOwl( owlConfiguration ) {
+    var $owl = $("<div class='cage'></div>")[0];
+    
+    
+    return $owl;
+
+}
+
+function makeBodyPart( data ) {
+
+}
+
